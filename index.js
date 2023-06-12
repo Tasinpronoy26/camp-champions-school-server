908820e77e1afb9dbd6c1fc673cddf3a0d698c74
Tasinpronoy26 <tasinpronoy56@gmail.com>
2023-06-12 17:51:59 +0600

    UPDATED VERCEL

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 //MONGODB CONNECT
 
 
-const uri = "mongodb+srv://tasinpronoy56:uw3RxRhGiqGg7QsI@cluster0.es0s7yl.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.es0s7yl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
